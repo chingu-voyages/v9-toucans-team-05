0e186d2ab56ea959abd9eedb25669021407eac29
TA <pepper.ppeatns@gmail.com>
2019-06-10 17:35:09 +0900

    Choose SearchEngine function enabled

--- js/ChooseEngine.js
+++ js/ChooseEngine.js
@@ -1,5 +1,53 @@
 $(function() {
+  $("#cse-search-box")
+    .on("mouseover focusin", function() {
+      $(".search-hover").css("border-bottom", "0.1rem solid #dee2e6");
+      $("#ChooseSearch").show();
+      $(".search-angle-i").show();
+    })
+    .on("focusout", function() {
+      $(".search-hover").css("border-bottom", "none");
+      $("#ChooseSearch").hide();
+      $(".search-angle-i").hide();
+    });
+
   $("#ChooseSearch").on("click", function() {
-    $("#Search-engine-modal").show();
+    if ($(this).hasClass("active")) {
+      $(".active").removeClass("active");
+      $("#Search-engine-modal").hide();
+    } else {
+      $(this).addClass("active");
+      $("#Search-engine-modal").show();
+    }
+  });
+
+  $(".searchtype").on("click", function() {
+    var id = $(this).attr("id");
+    switch (id) {
+      case "bing":
+        $("#cse-search-box").attr("action", "https://www.bing.com/");
+        $("#ChooseSearch").attr(
+          "src",
+          "./img/logo/bing_icon-icons.com_62711.svg"
+        );
+        break;
+      case "google":
+        $("#cse-search-box").attr("action", "http://google.com/cse");
+        $("#ChooseSearch").attr(
+          "src",
+          "./img/logo/1492616990-1-google-search-logo-engine-service-suits_83412.svg"
+        );
+        break;
+      case "DuckDuckGo":
+        $("#cse-search-box").attr("action", "http://duckduckgo.com/");
+        $("#ChooseSearch").attr(
+          "src",
+          "./img/logo/DuckDuckGo_icon-icons.com_67089.svg"
+        );
+        break;
+    }
+    $(".active").removeClass("active");
+    $("#Search-engine-modal").hide();
+    $("#search-input").focus();
   });
 });
