068e3f1ef076059a5d96d42cf044f9ea0f306eb8
Josy Hartig <josy_hartig@yahoo.de>
2019-06-14 17:54:15 +0800

    updated weather element

--- js/weather.js
+++ js/weather.js
@@ -23,24 +23,74 @@ if (navigator.geolocation) {
 }
 
 function showPosition(position) {
-  var api = "https://fcc-weather-api.glitch.me/api/current?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude;
+  var geoApi = "https://api.codetabs.com/v1/proxy?quest=https://www.metaweather.com/api/location/search/?lattlong=" + position.coords.latitude + "," + position.coords.longitude;
 
-  $.getJSON(api, function(data){
-    $("#location").html(data.name);
-    $("#temperature").html(data.main.temp.toFixed(0) + "°");
+  $.getJSON(geoApi, function(data){
+    var weatherApi = "https://api.codetabs.com/v1/proxy?quest=https://www.metaweather.com/api/location/" + data[0].woeid;
+    $(".location").html(data[0].title);
+    $.getJSON(weatherApi, function(json){
+      $(".temperature").html(json.consolidated_weather[0].the_temp.toFixed(0) + "°");
+      $(".max-temperature").html(json.consolidated_weather[0].max_temp.toFixed(0) + "°");
+      $(".min-temperature").html(json.consolidated_weather[0].min_temp.toFixed(0) + "°");
 
-    switch (data.weather[0].main) {
-      case "Clouds": $("#weather-icon").html("<i class='wi wi-day-cloudy'></i>");
-      break;
-      case "Clear Sky": $("#weather-icon").html("<i class='wi wi-day-sunny'></i>");
-      break;
-      case "Rain": $("#weather-icon").html("<i class='wi wi-rain-wind'></i>");
-      break;
-      case "Thunderstorm": $("#weather-icon").html("<i class='wi wi-thunderstorm'></i>");
-      break;
-      case "Snow": $("#weather-icon").html("<i class='wi wi-snow'></i>");
-      break;
-      case "Mist": $("#weather-icon").html("<i class='wi wi-fog'></i>");
+
+      switch (json.consolidated_weather[0].weather_state_abbr) {
+        case "lc": $(".weather-icon").html("<i class='wi wi-day-cloudy'></i>");
+        break;
+        case "hc": $(".weather-icon").html("<i class='wi wi-cloudy'></i>");
+        break;
+        case "c": $(".weather-icon").html("<i class='wi wi-day-sunny'></i>");
+        break;
+        case "hr": $(".weather-icon").html("<i class='wi wi-rain-wind'></i>");
+        break;
+        case "lr": $(".weather-icon").html("<i class='wi wi-rain-wind'></i>");
+        break;
+        case "t": $(".weather-icon").html("<i class='wi wi-thunderstorm'></i>");
+        break;
+        case "sn": $(".weather-icon").html("<i class='wi wi-snow'></i>");
+        break;
+        case "sl": $(".weather-icon").html("<i class='wi wi-sleet'></i>");
+        break;
+        case "h": $(".weather-icon").html("<i class='wi wi-hail'></i>");
+        break;
+        case "s": $(".weather-icon").html("<i class='wi wi-day-rain'></i>");
+        break;
     }
+
+          var output = '';
+          for (var i = 0; i < json.consolidated_weather.length; i++) {
+            output += '<div class="weekday"></div><div class="current-temp-icon">' +
+            '<span class ="weather-icon" id="forecast-weather-icon"></span>' +
+            '<span class="max-temperature" id="max-temperature"></span>' +
+            '<span class="min-temperature" id="min-temperature">' +
+            '</span></div>';
+
+            switch (json.consolidated_weather[i].weather_state_abbr) {
+              case "lc": $(".weather-icon").html("<i class='wi wi-day-cloudy'></i>");
+              break;
+              case "hc": $(".weather-icon").html("<i class='wi wi-cloudy'></i>");
+              break;
+              case "c": $(".weather-icon").html("<i class='wi wi-day-sunny'></i>");
+              break;
+              case "hr": $(".weather-icon").html("<i class='wi wi-rain-wind'></i>");
+              break;
+              case "lr": $(".weather-icon").html("<i class='wi wi-rain-wind'></i>");
+              break;
+              case "t": $(".weather-icon").html("<i class='wi wi-thunderstorm'></i>");
+              break;
+              case "sn": $(".weather-icon").html("<i class='wi wi-snow'></i>");
+              break;
+              case "sl": $(".weather-icon").html("<i class='wi wi-sleet'></i>");
+              break;
+              case "h": $(".weather-icon").html("<i class='wi wi-hail'></i>");
+              break;
+              case "s": $(".weather-icon").html("<i class='wi wi-day-rain'></i>");
+              break;
+          }
+          }
+          $(".day").html(output);
+
+
   });
-}
+  });
+  }
