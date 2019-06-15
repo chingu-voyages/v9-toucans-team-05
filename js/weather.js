296699a5345bc4c4b0400bea9f2d0b703071419d
Josy Hartig <josy_hartig@yahoo.de>
2019-06-15 22:47:38 +0800

    get user location without permission

--- js/weather.js
+++ js/weather.js
@@ -16,12 +16,6 @@ if (hour >= 4 && hour < 12) {
 }
 
 // Weather
-if (navigator.geolocation) {
- navigator.geolocation.getCurrentPosition(showPosition);
-} else {
- alert('Geolocation is not supported in your browser');
-}
-
 var weatherDescriptionTwo;
 var weatherDescriptionThree;
 var weatherDescriptionFour;
@@ -30,155 +24,156 @@ var weatherDescription;
 var temp;
 var tempMin;
 
-function showPosition(position) {
-  var geoApi = "https://api.codetabs.com/v1/proxy?quest=https://www.metaweather.com/api/location/search/?lattlong=" + position.coords.latitude + "," + position.coords.longitude;
-
-  $.getJSON(geoApi, function(data){
-    var weatherApi = "https://api.codetabs.com/v1/proxy?quest=https://www.metaweather.com/api/location/" + data[0].woeid;
-    $(".location").html(data[0].title);
-    $.getJSON(weatherApi, function(json){
-      $(".temperature").html(json.consolidated_weather[0].the_temp.toFixed(0) + "°");
-      $(".max-temperature").html(json.consolidated_weather[0].max_temp.toFixed(0) + "°");
-      $(".min-temperature").html(json.consolidated_weather[0].min_temp.toFixed(0) + "°");
-      $(".max-temperature-two").html(json.consolidated_weather[1].max_temp.toFixed(0) + "°");
-      $(".min-temperature-two").html(json.consolidated_weather[1].min_temp.toFixed(0) + "°");
-      $(".max-temperature-three").html(json.consolidated_weather[2].max_temp.toFixed(0) + "°");
-      $(".min-temperature-three").html(json.consolidated_weather[2].min_temp.toFixed(0) + "°");
-      $(".max-temperature-four").html(json.consolidated_weather[3].max_temp.toFixed(0) + "°");
-      $(".min-temperature-four").html(json.consolidated_weather[3].min_temp.toFixed(0) + "°");
-      $(".max-temperature-five").html(json.consolidated_weather[4].max_temp.toFixed(0) + "°");
-      $(".min-temperature-five").html(json.consolidated_weather[4].min_temp.toFixed(0) + "°");
-      $(".weather-description").html(json.consolidated_weather[0].weather_state_name);
-      weatherDescriptionTwo = json.consolidated_weather[1].weather_state_name;
-      weatherDescriptionThree = json.consolidated_weather[2].weather_state_name;
-      weatherDescriptionFour = json.consolidated_weather[3].weather_state_name;
-      weatherDescriptionFive = json.consolidated_weather[4].weather_state_name;
-      weatherDescription = json.consolidated_weather[0].weather_state_name;
-      temp = json.consolidated_weather[0].the_temp.toFixed(0) + "°";
-      tempTwo = json.consolidated_weather[1].max_temp.toFixed(0) + "°";
-      tempMinTwo = json.consolidated_weather[1].min_temp.toFixed(0) + "°";
-      tempThree = json.consolidated_weather[2].max_temp.toFixed(0) + "°";
-      tempMinThree = json.consolidated_weather[2].min_temp.toFixed(0) + "°";
-      tempFour = json.consolidated_weather[3].max_temp.toFixed(0) + "°";
-      tempMinFour = json.consolidated_weather[3].min_temp.toFixed(0) + "°";
-      tempFive = json.consolidated_weather[4].max_temp.toFixed(0) + "°";
-      tempMinFive = json.consolidated_weather[4].min_temp.toFixed(0) + "°";
-
+$.getJSON('https://geoip-db.com/json/', function(position){
+    var geoApi = "https://api.codetabs.com/v1/proxy?quest=https://www.metaweather.com/api/location/search/?lattlong=" + position.latitude + "," + position.longitude;
+    $.getJSON(geoApi, function(data){
+      var weatherApi = "https://api.codetabs.com/v1/proxy?quest=https://www.metaweather.com/api/location/" + data[0].woeid;
+      $(".location").html(data[0].title);
+      $.getJSON(weatherApi, function(json){
+        $(".temperature").html(json.consolidated_weather[0].the_temp.toFixed(0) + "°");
+        $(".max-temperature").html(json.consolidated_weather[0].max_temp.toFixed(0) + "°");
+        $(".min-temperature").html(json.consolidated_weather[0].min_temp.toFixed(0) + "°");
+        $(".max-temperature-two").html(json.consolidated_weather[1].max_temp.toFixed(0) + "°");
+        $(".min-temperature-two").html(json.consolidated_weather[1].min_temp.toFixed(0) + "°");
+        $(".max-temperature-three").html(json.consolidated_weather[2].max_temp.toFixed(0) + "°");
+        $(".min-temperature-three").html(json.consolidated_weather[2].min_temp.toFixed(0) + "°");
+        $(".max-temperature-four").html(json.consolidated_weather[3].max_temp.toFixed(0) + "°");
+        $(".min-temperature-four").html(json.consolidated_weather[3].min_temp.toFixed(0) + "°");
+        $(".max-temperature-five").html(json.consolidated_weather[4].max_temp.toFixed(0) + "°");
+        $(".min-temperature-five").html(json.consolidated_weather[4].min_temp.toFixed(0) + "°");
+        $(".weather-description").html(json.consolidated_weather[0].weather_state_name);
+        weatherDescriptionTwo = json.consolidated_weather[1].weather_state_name;
+        weatherDescriptionThree = json.consolidated_weather[2].weather_state_name;
+        weatherDescriptionFour = json.consolidated_weather[3].weather_state_name;
+        weatherDescriptionFive = json.consolidated_weather[4].weather_state_name;
+        weatherDescription = json.consolidated_weather[0].weather_state_name;
+        temp = json.consolidated_weather[0].the_temp.toFixed(0) + "°";
+        tempTwo = json.consolidated_weather[1].max_temp.toFixed(0) + "°";
+        tempMinTwo = json.consolidated_weather[1].min_temp.toFixed(0) + "°";
+        tempThree = json.consolidated_weather[2].max_temp.toFixed(0) + "°";
+        tempMinThree = json.consolidated_weather[2].min_temp.toFixed(0) + "°";
+        tempFour = json.consolidated_weather[3].max_temp.toFixed(0) + "°";
+        tempMinFour = json.consolidated_weather[3].min_temp.toFixed(0) + "°";
+        tempFive = json.consolidated_weather[4].max_temp.toFixed(0) + "°";
+        tempMinFive = json.consolidated_weather[4].min_temp.toFixed(0) + "°";
 
 
-      switch (json.consolidated_weather[0].weather_state_abbr) {
-        case "lc": $(".weather-icon-one").html("<i class='wi wi-day-cloudy'></i>");
-        break;
-        case "hc": $(".weather-icon-one").html("<i class='wi wi-cloudy'></i>");
-        break;
-        case "c": $(".weather-icon-one").html("<i class='wi wi-day-sunny'></i>");
-        break;
-        case "hr": $(".weather-icon-one").html("<i class='wi wi-rain-wind'></i>");
-        break;
-        case "lr": $(".weather-icon-one").html("<i class='wi wi-rain-wind'></i>");
-        break;
-        case "t": $(".weather-icon-one").html("<i class='wi wi-thunderstorm'></i>");
-        break;
-        case "sn": $(".weather-icon-one").html("<i class='wi wi-snow'></i>");
-        break;
-        case "sl": $(".weather-icon-one").html("<i class='wi wi-sleet'></i>");
-        break;
-        case "h": $(".weather-icon-one").html("<i class='wi wi-hail'></i>");
-        break;
-        case "s": $(".weather-icon-one").html("<i class='wi wi-day-rain'></i>");
-        break;
-    }
-      switch (json.consolidated_weather[1].weather_state_abbr) {
-        case "lc": $(".weather-icon-two").html("<i class='wi wi-day-cloudy'></i>");
-        break;
-        case "hc": $(".weather-icon-two").html("<i class='wi wi-cloudy'></i>");
-        break;
-        case "c": $(".weather-icon-two").html("<i class='wi wi-day-sunny'></i>");
-        break;
-        case "hr": $(".weather-icon-two").html("<i class='wi wi-rain-wind'></i>");
-        break;
-        case "lr": $(".weather-icon-two").html("<i class='wi wi-rain-wind'></i>");
-        break;
-        case "t": $(".weather-icon-two").html("<i class='wi wi-thunderstorm'></i>");
-        break;
-        case "sn": $(".weather-icon-two").html("<i class='wi wi-snow'></i>");
-        break;
-        case "sl": $(".weather-icon-two").html("<i class='wi wi-sleet'></i>");
-        break;
-        case "h": $(".weather-icon-two").html("<i class='wi wi-hail'></i>");
-        break;
-        case "s": $(".weather-icon-two").html("<i class='wi wi-day-rain'></i>");
-        break;
-    }
-      switch (json.consolidated_weather[2].weather_state_abbr) {
-        case "lc": $(".weather-icon-three").html("<i class='wi wi-day-cloudy'></i>");
-        break;
-        case "hc": $(".weather-icon-three").html("<i class='wi wi-cloudy'></i>");
-        break;
-        case "c": $(".weather-icon-three").html("<i class='wi wi-day-sunny'></i>");
-        break;
-        case "hr": $(".weather-icon-three").html("<i class='wi wi-rain-wind'></i>");
-        break;
-        case "lr": $(".weather-icon-three").html("<i class='wi wi-rain-wind'></i>");
-        break;
-        case "t": $(".weather-icon-three").html("<i class='wi wi-thunderstorm'></i>");
-        break;
-        case "sn": $(".weather-icon-three").html("<i class='wi wi-snow'></i>");
-        break;
-        case "sl": $(".weather-icon-three").html("<i class='wi wi-sleet'></i>");
-        break;
-        case "h": $(".weather-icon-three").html("<i class='wi wi-hail'></i>");
-        break;
-        case "s": $(".weather-icon-three").html("<i class='wi wi-day-rain'></i>");
-        break;
-    }
-      switch (json.consolidated_weather[3].weather_state_abbr) {
-        case "lc": $(".weather-icon-four").html("<i class='wi wi-day-cloudy'></i>");
-        break;
-        case "hc": $(".weather-icon-four").html("<i class='wi wi-cloudy'></i>");
-        break;
-        case "c": $(".weather-icon-four").html("<i class='wi wi-day-sunny'></i>");
-        break;
-        case "hr": $(".weather-icon-four").html("<i class='wi wi-rain-wind'></i>");
-        break;
-        case "lr": $(".weather-icon-four").html("<i class='wi wi-rain-wind'></i>");
-        break;
-        case "t": $(".weather-icon-four").html("<i class='wi wi-thunderstorm'></i>");
-        break;
-        case "sn": $(".weather-icon-four").html("<i class='wi wi-snow'></i>");
-        break;
-        case "sl": $(".weather-icon-four").html("<i class='wi wi-sleet'></i>");
-        break;
-        case "h": $(".weather-icon-four").html("<i class='wi wi-hail'></i>");
-        break;
-        case "s": $(".weather-icon-four").html("<i class='wi wi-day-rain'></i>");
-        break;
+
+        switch (json.consolidated_weather[0].weather_state_abbr) {
+          case "lc": $(".weather-icon-one").html("<i class='wi wi-day-cloudy'></i>");
+          break;
+          case "hc": $(".weather-icon-one").html("<i class='wi wi-cloudy'></i>");
+          break;
+          case "c": $(".weather-icon-one").html("<i class='wi wi-day-sunny'></i>");
+          break;
+          case "hr": $(".weather-icon-one").html("<i class='wi wi-rain-wind'></i>");
+          break;
+          case "lr": $(".weather-icon-one").html("<i class='wi wi-rain-wind'></i>");
+          break;
+          case "t": $(".weather-icon-one").html("<i class='wi wi-thunderstorm'></i>");
+          break;
+          case "sn": $(".weather-icon-one").html("<i class='wi wi-snow'></i>");
+          break;
+          case "sl": $(".weather-icon-one").html("<i class='wi wi-sleet'></i>");
+          break;
+          case "h": $(".weather-icon-one").html("<i class='wi wi-hail'></i>");
+          break;
+          case "s": $(".weather-icon-one").html("<i class='wi wi-day-rain'></i>");
+          break;
       }
-      switch (json.consolidated_weather[4].weather_state_abbr) {
-        case "lc": $(".weather-icon-five").html("<i class='wi wi-day-cloudy'></i>");
-        break;
-        case "hc": $(".weather-icon-five").html("<i class='wi wi-cloudy'></i>");
-        break;
-        case "c": $(".weather-icon-five").html("<i class='wi wi-day-sunny'></i>");
-        break;
-        case "hr": $(".weather-icon-five").html("<i class='wi wi-rain-wind'></i>");
-        break;
-        case "lr": $(".weather-icon-five").html("<i class='wi wi-rain-wind'></i>");
-        break;
-        case "t": $(".weather-icon-five").html("<i class='wi wi-thunderstorm'></i>");
-        break;
-        case "sn": $(".weather-icon-five").html("<i class='wi wi-snow'></i>");
-        break;
-        case "sl": $(".weather-icon-five").html("<i class='wi wi-sleet'></i>");
-        break;
-        case "h": $(".weather-icon-five").html("<i class='wi wi-hail'></i>");
-        break;
-        case "s": $(".weather-icon-five").html("<i class='wi wi-day-rain'></i>");
-        break;
+        switch (json.consolidated_weather[1].weather_state_abbr) {
+          case "lc": $(".weather-icon-two").html("<i class='wi wi-day-cloudy'></i>");
+          break;
+          case "hc": $(".weather-icon-two").html("<i class='wi wi-cloudy'></i>");
+          break;
+          case "c": $(".weather-icon-two").html("<i class='wi wi-day-sunny'></i>");
+          break;
+          case "hr": $(".weather-icon-two").html("<i class='wi wi-rain-wind'></i>");
+          break;
+          case "lr": $(".weather-icon-two").html("<i class='wi wi-rain-wind'></i>");
+          break;
+          case "t": $(".weather-icon-two").html("<i class='wi wi-thunderstorm'></i>");
+          break;
+          case "sn": $(".weather-icon-two").html("<i class='wi wi-snow'></i>");
+          break;
+          case "sl": $(".weather-icon-two").html("<i class='wi wi-sleet'></i>");
+          break;
+          case "h": $(".weather-icon-two").html("<i class='wi wi-hail'></i>");
+          break;
+          case "s": $(".weather-icon-two").html("<i class='wi wi-day-rain'></i>");
+          break;
       }
-  });
-  });
-  }
+        switch (json.consolidated_weather[2].weather_state_abbr) {
+          case "lc": $(".weather-icon-three").html("<i class='wi wi-day-cloudy'></i>");
+          break;
+          case "hc": $(".weather-icon-three").html("<i class='wi wi-cloudy'></i>");
+          break;
+          case "c": $(".weather-icon-three").html("<i class='wi wi-day-sunny'></i>");
+          break;
+          case "hr": $(".weather-icon-three").html("<i class='wi wi-rain-wind'></i>");
+          break;
+          case "lr": $(".weather-icon-three").html("<i class='wi wi-rain-wind'></i>");
+          break;
+          case "t": $(".weather-icon-three").html("<i class='wi wi-thunderstorm'></i>");
+          break;
+          case "sn": $(".weather-icon-three").html("<i class='wi wi-snow'></i>");
+          break;
+          case "sl": $(".weather-icon-three").html("<i class='wi wi-sleet'></i>");
+          break;
+          case "h": $(".weather-icon-three").html("<i class='wi wi-hail'></i>");
+          break;
+          case "s": $(".weather-icon-three").html("<i class='wi wi-day-rain'></i>");
+          break;
+      }
+        switch (json.consolidated_weather[3].weather_state_abbr) {
+          case "lc": $(".weather-icon-four").html("<i class='wi wi-day-cloudy'></i>");
+          break;
+          case "hc": $(".weather-icon-four").html("<i class='wi wi-cloudy'></i>");
+          break;
+          case "c": $(".weather-icon-four").html("<i class='wi wi-day-sunny'></i>");
+          break;
+          case "hr": $(".weather-icon-four").html("<i class='wi wi-rain-wind'></i>");
+          break;
+          case "lr": $(".weather-icon-four").html("<i class='wi wi-rain-wind'></i>");
+          break;
+          case "t": $(".weather-icon-four").html("<i class='wi wi-thunderstorm'></i>");
+          break;
+          case "sn": $(".weather-icon-four").html("<i class='wi wi-snow'></i>");
+          break;
+          case "sl": $(".weather-icon-four").html("<i class='wi wi-sleet'></i>");
+          break;
+          case "h": $(".weather-icon-four").html("<i class='wi wi-hail'></i>");
+          break;
+          case "s": $(".weather-icon-four").html("<i class='wi wi-day-rain'></i>");
+          break;
+        }
+        switch (json.consolidated_weather[4].weather_state_abbr) {
+          case "lc": $(".weather-icon-five").html("<i class='wi wi-day-cloudy'></i>");
+          break;
+          case "hc": $(".weather-icon-five").html("<i class='wi wi-cloudy'></i>");
+          break;
+          case "c": $(".weather-icon-five").html("<i class='wi wi-day-sunny'></i>");
+          break;
+          case "hr": $(".weather-icon-five").html("<i class='wi wi-rain-wind'></i>");
+          break;
+          case "lr": $(".weather-icon-five").html("<i class='wi wi-rain-wind'></i>");
+          break;
+          case "t": $(".weather-icon-five").html("<i class='wi wi-thunderstorm'></i>");
+          break;
+          case "sn": $(".weather-icon-five").html("<i class='wi wi-snow'></i>");
+          break;
+          case "sl": $(".weather-icon-five").html("<i class='wi wi-sleet'></i>");
+          break;
+          case "h": $(".weather-icon-five").html("<i class='wi wi-hail'></i>");
+          break;
+          case "s": $(".weather-icon-five").html("<i class='wi wi-day-rain'></i>");
+          break;
+        }
+    });
+    });
+    }
+);
+
 
 // Days of the week
     $(".weekday-one").html(moment().format('ddd').toUpperCase());
