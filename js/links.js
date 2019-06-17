eb7d5098bd0608a531453568c339f4858f1c4e85
Josy Hartig <josy_hartig@yahoo.de>
2019-06-17 18:19:56 +0800

    added basic functionality and styling to link section

--- js/links.js
+++ js/links.js
@@ -4,7 +4,18 @@ $('.links').click(function(){
   $('.custom-links-container').toggle();
 });
 
-$('.add-new-link').click(function(){
+$(document).on('click', '.add-new-link', function() {
   $('.add-new-link').remove();
-  $('.link-list').append('<li><input type="text" id="idea" placeholder="Name"></li><li><input type="text" id="idea" placeholder="URL"></li>');
+  $('.link-list').append('<li><form class="link-form"><input class="input-link" type="text" id="idea" placeholder="Name" onsubmit="return false"><button type="button" class="remove"><i class="icon ion-md-close"></i></button><br><input class="input-link" type="text" id="idea" placeholder="URL"></form></li>');
+});
+
+$(document).on('click', '.remove', function() {
+  $('.link-form').remove();
+  $('.link-list').append($('<li class="add-new-link">New Link</li>'));
+});
+
+$('.link-list').on("keypress", function(e){
+    if(e.which == 13){
+        alert("You've pressed the enter key!");
+    }
 });
