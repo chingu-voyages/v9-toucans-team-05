c845ac71d9c0ef00070fea47c22f8dfc43770ac3
Josy Hartig <josy_hartig@yahoo.de>
2019-06-24 18:49:57 +0800

    added tweet option & saved quotes list

--- js/quote.js
+++ js/quote.js
@@ -1,5 +1,8 @@
 var savedQuote = window.localStorage.getItem('quote');
 var savedAuthor = window.localStorage.getItem('author');
+var savedHeart = window.localStorage.getItem('heart');
+var savedQuotes = window.localStorage.getItem('likedQuotes')
+
 var day = new Date();
 var quoteDay = day.getDate() + "/" + (day.getMonth() + 1 );
 
@@ -8,6 +11,11 @@ $(document).ready(function() {
   if ("quote" in localStorage) {
     $('.quote').text(savedQuote);
     $('.author').text(savedAuthor);
+    if("heart" in localStorage) {
+      $('.heart').html(savedHeart);
+    } else {
+      $('.heart').html('<i class="far fa-heart like">');
+    }
   } else {
     $.getJSON('https://api.quotable.io/random', function(quote) {
       $('.quote').text('"' + quote.content + '"');
@@ -19,14 +27,41 @@ $(document).ready(function() {
   }
 });
 
+$(document).ready(function() {
+  if ("likedQuotes" in localStorage) {
+    $('.liked-quotes-container').html(savedQuotes);
+  } else {
+    $('.liked-quotes-container').append("<p style='color: rgba(237, 237, 237, 0.6)'>You haven't liked any quotes yet</p>");
+  }
+});
+
 // Clear localStorage from quote on new day -> one quote per day
 var savedQuoteDay = window.localStorage.getItem('quote-day');
 var now = day.getDate() + "/" + (day.getMonth() + 1 );
 
-function deleteFocus() {
+function deleteQuote() {
   if (savedQuoteDay != now) {
     localStorage.removeItem('quote');
     localStorage.removeItem('author');
+    localStorage.removeItem('heart')
   }
 }
-deleteFocus();
+deleteQuote();
+
+// Like quote
+$(document).on('click', '.like', function() {
+  $('.fa-heart').removeClass('far like').addClass('fas liked');
+  window.localStorage.setItem('heart', $('.heart').html());
+  $('.liked-quotes-container').append($('.quote').text() + ' ' + $('.author').text() + ' <i class="fas fa-heart"></i><hr>');
+  window.localStorage.setItem('likedQuotes', $('.liked-quotes-container').html());
+});
+
+// Tweet quote
+$(document).ready(function() {
+  $('.twitter a').attr('href', ($('.twitter a').attr('href') + savedQuote + '  —  ' + savedAuthor));
+});
+
+// View liked quotes
+$('.liked-quotes').click(function(){
+  $('.liked-quotes-container').slideToggle();
+})
