8b4434b0b14633a18ebf0fa63973a5d80eb08f7c
TA <pepper.ppeatns@gmail.com>
2019-07-06 22:52:45 +0900

    set all things back where I commited

--- js/Background.js
+++ js/Background.js
@@ -1,7 +1,7 @@
 function intRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-console.log(test_key);
+
 function FetchItems() {
   ("use strict");
   var value = "World Heritage";
