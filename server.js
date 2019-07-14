e9993e0fa3558c0762bfcb4fb0d6b98a554acff7
TA <pepper.ppeatns@gmail.com>
2019-07-14 18:07:24 +0900

    display photo location if data exists

--- server.js
+++ server.js
@@ -9,44 +9,48 @@ app.set('views',path.join(__dirname,'./'));
 app.set('view engine','ejs');
 app.use('/static',express.static(path.join(__dirname,"static")));
 app.use('/js',express.static(path.join(__dirname,"js")))
+app.use('/img',express.static(path.join(__dirname,"img")))
 
-function createURL(value) {
-    const BaseUrl = 'https://api.unsplash.com/search/photos?page=1',
-      keyword = "&query=" + encodeURIComponent(value),
-      APIkey ='&client_id=' + process.env.US_AcsKey,
-      url = BaseUrl + keyword + APIkey;
-    return url;
-  }
-const imgBaseURL=createURL('World Heritage');
-
-async function fetchURL() {
-    const res = await fetch(imgBaseURL);
-    data = await res.json();
-    return data;
-  }
-  const result=[],
-  imgURL=[],
-  Name=[],
-  Location=[];
-
+app.get('/', (req, res) => {
+  function createURL() {
+    //API base URL to get random photo
+    const collectionID='6809020,6091305',
+    BaseUrl ='https://api.unsplash.com//photos/random/?',
+    collections ='collections='+collectionID
+    options = '&featured=true&orientation=landscape',
+    APIkey = '&client_id=' + process.env.US_AcsKey,
+        url = BaseUrl + collections + options + APIkey;
+      return url;
+    }
+    let imgBaseURL=createURL();
+  
+  async function fetchURL() {
+      const res = await fetch(imgBaseURL);
+      data = await res.json();
+      return data;
+    }
+  
   fetchURL()
   .then(function(data) {
-    dt=data.results;
-    for(i=0;i<dt.length;i++){
-      imgURL[i] = dt[i].urls.raw+"&w=1600";
-      Name[i]=dt[i].user.name;
-      Location[i]=dt[i].user.location;  
-      result.push({img:imgURL[i],author:Name[i],location:Location[i]});
+    if(typeof(data.location)!=="undefined"){
+      res.render('index',
+      { img: data.urls.raw+"&w=1600",
+        author:data.user.name,
+        location:data.location.city,
+        link:data.links.download});
+    }else{
+      res.render('index',
+      { img: data.urls.raw+"&w=1600",
+        author:data.user.name,
+        location:'',
+        link:data.links.download});
     }
   })
   .catch(function(err) {
     console.log(err);
   });
-    app.get('/', (req, res) => {
-      let n = Math.floor(Math.random() * 10);
-      res.render('index',{img:result[n]['img'],author:result[n]['author'],location:result[n]['location']});
-    })
-
+  
+})
 
 const server = http.createServer(app);
 // Listen to the App Engine-specified port, or 8080 otherwise
